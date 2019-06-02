e0bec26e9a961d81a13998bfc7bc6eb28b55fe9d
Alan Turing <freemasl.sf@gmail.com>
2019-06-01 18:17:37 -0600

    Add clear tasks in Nav and disable clear button

--- main.js
+++ main.js
@@ -4,25 +4,26 @@ var titleInput = document.querySelector('#js-input-title');
 var itemInput = document.querySelector('#js-input-item');
 var addBtn = document.querySelector('#js-plus-btn');
 var makeTaskBtn = document.querySelector('#js-make-task-btn');
-var cleanBtn = document.querySelector('#js-clear-btn');
+var clearBtn = document.querySelector('#js-clear-btn');
 var filterBtn = document.querySelector('#js-filter-btn');
 var urgentBtn = document.querySelector('#js-urgent-btn');
 var deleteBtn = document.querySelector('#js-delete-btn');
 var cardSection = document.querySelector('#js-card-section');
 var taskList = document.querySelector('#js-task-list');
 addBtn.disabled = true;
+clearBtn.disabled = true;
 
 // cardSection.addEventListener('click',handleCardActions);
 addBtn.addEventListener('click', addTaskList);
 taskList.addEventListener('click', removeFromTaskList)
 // makeTaskBtn.addEventListener('click', );
-// cleanBtn.addEventListener('click', );
+clearBtn.addEventListener('click', clearNav);
 // filterBtn.addEventListener('click', );
 // urgentBtn.addEventListener('click', );
 // deleteBtn.addEventListener('click', );
 // searchInput.addEventListener('keyup', );
-// titleInput.addEventListener('keyup', );
-itemInput.addEventListener('keyup', disableBtns);
+titleInput.addEventListener('keyup', disableClearBtn);
+itemInput.addEventListener('keyup', disableAddBtn);
 
 function addTaskList() {
   taskList.insertAdjacentHTML('beforeend', `<li class='nav__container--bullet'>${itemInput.value}</li>`);
@@ -30,14 +31,23 @@ function addTaskList() {
   addBtn.disabled = true;
 };
 
-function disableBtns() {
-  var emptyInput = itemInput.value === '';
-  addBtn.disabled = emptyInput;
+function disableAddBtn() {
+  var emptyItemInput = itemInput.value === '';
+  addBtn.disabled = emptyItemInput;
 };
 
 function removeFromTaskList(e) {
   if (e.target.className === 'nav__container--bullet') {
-    console.log('e.target');
     e.target.remove();
   }
 };
+
+function clearNav() {
+  location.reload(true);
+}
+
+function disableClearBtn() {
+  var allEmptyInputs = titleInput.value === '';
+  clearBtn.disabled = allEmptyInputs;
+}
+
